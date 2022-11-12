f0bc918dfa924c3392512dabec848e6861ac109e
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-11-12 12:51:45 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,22 +17,32 @@ export default function IndexPage() {
       </Head>
       
       <h1>Maliek Blog</h1>
-      
-        <ul>
-          <li>
-            On browsers that support <code>prefers-color-scheme</code> and report the user prefers <code>dark</code>,
-            just obey and don't give the user an override option, since they clearly state they like dark.
-          </li>
-          <li>
-            On browsers that support <code>prefers-color-scheme</code> and report the user prefers <code>light</code>
-            or <code>no-preference</code>, offer the option to toggle dark mode manually.
-          </li>
-          <li>
-            On browsers that don't support <code>prefers-color-scheme</code>,
-            offer the option to toggle dark mode manually.
-          </li>
-        </ul>
-  
+      <p>Lorem ipsum dolor sit amet, voluptua oporteat cum cu, eu vix interesset scribentur. Et iudico efficiendi duo, vim an odio iusto reformidans. At maluisset scripserit duo, cum in sumo delenit. Quot erant maiestatis usu ut. Ne mea maiorum vivendum argumentum, tale utinam postea mei in, eam in odio dicta patrioque.
+      </p>
+      <p>
+      Cum ad graeci legimus, ut duo omnesque petentium, mei saepe explicari principes eu. Ex mei ocurreret expetendis, ex errem blandit has. Phaedrum mediocrem appellantur his ut. Erat viris mediocrem id ius, ad dicta appareat assentior sit. Nec et nullam singulis moderatius, est reque vidisse copiosae eu.
+      </p>
+      <p>
+      Mutat dolores ea duo. Usu cu dico everti tincidunt, pri vero nullam vidisse no. Eam novum aeque populo cu. Verear contentiones ne quo, no sed consulatu definitionem.
+      </p>
+      <p>
+      Cum ad graeci legimus, ut duo omnesque petentium, mei saepe explicari principes eu. Ex mei ocurreret expetendis, ex errem blandit has. Phaedrum mediocrem appellantur his ut. Erat viris mediocrem id ius, ad dicta appareat assentior sit. Nec et nullam singulis moderatius, est reque vidisse copiosae eu.</p>
+      <p>
+      Mutat dolores ea duo. Usu cu dico everti tincidunt, pri vero nullam vidisse no. Eam novum aeque populo cu. Verear contentiones ne quo, no sed consulatu definitionem.
+      </p>
+      <p>
+      Cum ad graeci legimus, ut duo omnesque petentium, mei saepe explicari principes eu. Ex mei ocurreret expetendis, ex errem blandit has. Phaedrum mediocrem appellantur his ut. Erat viris mediocrem id ius, ad dicta appareat assentior sit. Nec et nullam singulis moderatius, est reque vidisse copiosae eu.</p>
+      <p>
+      Mutat dolores ea duo. Usu cu dico everti tincidunt, pri vero nullam vidisse no. Eam novum aeque populo cu. Verear contentiones ne quo, no sed consulatu definitionem.
+      </p>
+      <p class="heading">
+      <amp-social-share type="twitter" width="45" height="33"></amp-social-share>
+      <amp-social-share type="facebook" width="45" height="33" data-attribution="254325784911610"></amp-social-share>
+      <amp-social-share type="gplus" width="45" height="33"></amp-social-share>
+      <amp-social-share type="email" width="45" height="33"></amp-social-share>
+      <amp-social-share type="pinterest" width="45" height="33"></amp-social-share>
+      </p>
+        
       <style jsx>
 {`
 
